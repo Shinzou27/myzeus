4961f5bb17416e0fc0607159383dfae533428146
Felipe <felipecassianobarbosa@hotmail.com>
2023-12-07 11:40:34 -0300

    feat: login with auth, better feedback msgs

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
+import jsonwebtoken from 'jsonwebtoken';
 import { connection } from "../database/connection";
 import bcrypt from 'bcrypt'
 export class UserController {
     async create(req: Request, res: Response) {
-        const {username, password} = req.body;
+        const { username, password } = req.body;
         const hash = await bcrypt.hash(password as string, 12);
         try {
             const user = await connection.user.create({
@@ -12,10 +13,10 @@ export class UserController {
                     password: hash
                 }
             })
-            return res.json({message: 'Cadastro realizado com sucesso!', type: 'success'});
-        } catch(e: any) {
+            return res.json({ message: 'Cadastro realizado com sucesso!', type: 'success' });
+        } catch (e: any) {
             console.log(e.message);
-            return res.json({message:'Falha no cadastro. Usuário já existe.', type: 'danger'})
+            return res.json({ message: 'Falha no cadastro. Usuário já existe.', type: 'danger' })
         };
     }
     async list(req: Request, res: Response) {
@@ -23,40 +24,62 @@ export class UserController {
         res.json(users);
     }
     async log(req: Request, res: Response) {
-        const {username, password} = req.query;
+        const { username, password } = req.body;
         if (!username || !password) {
-			res.send({message: 'Nome ou senha incorretos.'});
+            res.send({ message: 'Nome ou senha incorretos.' });
             return;
-		}
+        }
         const user = await connection.user.findUnique({
             where: {
-                username: req.query.username as string
+                username: username as string
             }
         });
-        if (!user) return res.send({message: 'Usuário não encontrado.'});
-        const validation = await bcrypt.compare(req.query.password as string, user.password);
-        if(!validation) return res.send({message: 'Senha incorreta.'});
-        return res.json(user);
+        if (!user) return res.send({ message: 'Usuário não encontrado.' });
+        const validation = await bcrypt.compare(password as string, user.password);
+        if (!validation) return res.send({ message: 'Senha incorreta.' });
+        const token = jsonwebtoken.sign({
+            id: user.id,
+            username: user.username,
+        }, process.env.JWT_SECRET!, {
+            expiresIn: '1d'
+        })
+        return res.json({
+            token: token,
+            id: user.id,
+            username: user.username
+        });
     }
     async updatePassword(req: Request, res: Response) {
-        const {id} = req.params;
+        const { id } = req.params;
         const intId = parseInt(id);
-        const {current} = req.body;
+        const { current } = req.body;
         const hash = await bcrypt.hash(req.body.new as string, 12);
-        const validation = await bcrypt.compare(current, hash);
-        if (validation) return res.json({message: 'Senhas iguais.'});
-        const user = await connection.user.update({
+        const inputValidation = await bcrypt.compare(current, hash);
+        if (inputValidation) return res.json({ message: 'Senhas iguais.', type: 'danger' });
+        const user = await connection.user.findUnique({
             where: {
                 id: intId
-            },
-            data: {
-                password: hash
             }
         });
-        return res.json(user);
+        if (user) {
+            const passwordValidation = await bcrypt.compare(current, user.password);
+            if (passwordValidation) {
+                await connection.user.update({
+                    where: {
+                        id: intId
+                    },
+                    data: {
+                        password: hash
+                    }
+                })
+                return res.json({ message: 'Senha atualizada!', type: 'success' });
+            }
+            return res.json({ message: 'A senha inserida não corresponde à sua senha atual.', type: 'danger' });
+        }
+        return res.json({ message: 'Usuário não encontrado.', type: 'danger' });
     }
     async delete(req: Request, res: Response) {
-        const {id} = req.params;
+        const { id } = req.params;
         const intId = parseInt(id);
         const user = await connection.user.delete({
             where: {
